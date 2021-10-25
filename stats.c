165024a34bac44e594f77a147b1dfbc8878b1a8b
vaishnavi-nayak-sujir <vaishnavinayak86@gmail.com>
2021-10-25 13:02:59 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -5,11 +5,13 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
-    int i,sum=0;
+    int i=0;
+    float sum=0.0;
     for (i=0;i<setlength;i++)
     {
         sum= sum+ numberset[i];
     }
+    s.average = (sum / setlength
 }
 
 int emailAlertCallCount = 0;
